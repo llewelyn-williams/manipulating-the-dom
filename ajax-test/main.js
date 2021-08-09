882df2fe60b19cd76b55f906aed2719894f5d04c
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-08-09 20:20:10 +0000

    test: Add code from follow along AJAX video
    Working With External Resources > Consuming APIs Using JavaScript >
    Timeout!
    Adding a setTimeout() to wait for readystate to be at 4.

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -4,14 +4,12 @@ var data;
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
-function setData(jsonData){
-    data = jsonData
-    console.log(data);
-}
-
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
+
+setTimeout(function(){
+    console.log(data);
+}, 500);
