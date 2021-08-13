1025b86dcb13ac89659fd0f26389f3c7dbff7916
llewelyn-williams <llewelyn.williams@googlemail.com>
2021-08-13 20:56:56 +0000

    test: Add code from follow along video AJAX cont.
    Working With External Resources > Data & The DOM >
    Tabular Data - Part One

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -17,14 +17,26 @@ function printDataToConsole(data){
     console.log(data);
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<td>${tableHeaders}</td>`;
+}
+
 function writeToDocument(type) {
     var el = document.getElementById("data");
     el.innerHTML = "";
     getData(type, function(data) {
         data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function (item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
+           // el.innerHTML += "<p>" + item.name + "</p>";
         });
-    })
+        el.innerHTML = `<table>${tableHeaders}</table>`
+    });
 }
